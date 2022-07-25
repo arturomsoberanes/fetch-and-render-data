d45cd2937d1d457f7702962a9976220689eb0247
Arturo Soberanes <arturo.soberanes01@gmail.com>
2022-07-25 14:52:52 -0500

    Change the data and styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,119 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
+
+const useDataApi = (initialUrl, initialData) => {
+  const { useState, useEffect, useReducer } = React;
+  const [url, setUrl] = useState(initialUrl);
+
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    isLoading: true,
+    isError: false,
+    data: initialData
+  });
+
+  useEffect(() => {
+    let didCancel = false;
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_INIT" });
+      try {
+        const result = await axios(url);
+        if (!didCancel) {
+          dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+        }
+      } catch (error) {
+        if (!didCancel) {
+          dispatch({ type: "FETCH_FAILURE" });
+        }
+      }
+    };
+    fetchData();
+    return () => {
+      didCancel = true;
+    };
+  }, [url]);
+  return [state, setUrl];
+};
+const dataFetchReducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_INIT":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false
+      };
+    case "FETCH_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload
+      };
+    case "FETCH_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true
+      };
+    default:
+      throw new Error();
+  }
+};
 function App() {
+  const { Fragment, useState, useEffect, useReducer } = React;
+  const [query, setQuery] = useState("pikachu");
+  const [{ data, isLoading, isError }, doFetch] = useDataApi(
+    "https://pokeapi.co/api/v2/pokemon/pikachu",
+    {
+      abilities: [],
+      name: [],
+      sprites: []
+    }
+  );
+  const {abilities:abilities, name:name, sprites:sprites} = data;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Fragment>
+      <div className="App">
+        <form
+          onSubmit={event => {
+            doFetch(`https://pokeapi.co/api/v2/pokemon/${query.toLowerCase()}`);
+
+            event.preventDefault();
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <input
+            type="text"
+            value={query}
+            onChange={event => setQuery(event.target.value)}
+          />
+          <button type="submit">Search</button>
+        </form>
+
+        {isError && <div>Something went wrong ...</div>}
+
+        {isLoading ? (
+          <div>Loading ...</div>
+        ) : (
+          <Card className="Card_Custom m-4">
+            <Card.Img src={sprites.front_default} variant="top"/>
+            <Card.Body className="border-top border-3 border-danger Card_Body_Custom">
+              <Card.Title>{name.toUpperCase()}</Card.Title>
+              <Card.Title className="fw-bold mb-1">Abilities:</Card.Title>
+              <ul className="list-group list-group-numbered">
+                {abilities.map( (ability, i) => (
+                  <li key={i} className="list-group-item mb-1">{ability.ability.name}</li>
+                ))}
+              </ul>
+            </Card.Body>
+          </Card>
+        )}
+      </div>
+    </Fragment>
   );
 }
 
