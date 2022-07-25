90c89e2835cb6531e59af737800d36e6a0b4d824
Arturo Soberanes <arturo.soberanes01@gmail.com>
2022-07-25 14:49:42 -0500

    Add Header

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './Header.css';
+
+function Header(){
+  return (
+    <header className="w-100 text-center mt-5">
+      <h1>Pokedex</h1>
+    </header>
+  );
+}
+
+export default Header;
